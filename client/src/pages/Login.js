3260d38aff53fb523d0941f9f14d39882a108905
Iris <iridian95@live.com>
2023-04-06 19:05:12 -0400

    adding Login function

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+function Login() {
+const [formState, setFormState]= useState ({email:'', pasword})
+const [login, { error }] = useMutation(LOGIN);
+
+}
+
+export default Login;
