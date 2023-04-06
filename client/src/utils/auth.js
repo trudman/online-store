6fbaa87749feb2eb9322f3ab2ed78629caba1721
Iris <iridian95@live.com>
2023-04-06 19:35:25 -0400

    adding token to local storage

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -22,7 +22,23 @@ class AuthService {
       return false;
     }
   }
- 
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
 }
 
 export default new AuthService();
