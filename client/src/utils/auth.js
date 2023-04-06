4d8a61f16040118a8c864e5148065766b11817ef
Iris <iridian95@live.com>
2023-04-06 19:33:47 -0400

    adding token validation function

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,5 +1,6 @@
 import decode from 'jwt-decode';
 
+// web token (JWT) retrieved 
 class AuthService {
   getProfile() {
     return decode(this.getToken());
@@ -8,9 +9,19 @@ class AuthService {
   loggedIn() {
  //if there's a token and stills valid 
     const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
+    return !!token && !this.tokenValidation(token);
   }
-
+// checks if token is valid or not
+  tokenValidation(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
  
 }
 
