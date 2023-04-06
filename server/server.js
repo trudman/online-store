eb22cc1a50186659655e17c1c0044be9c97fa603
Matthew C <cnymhw86@gmail.com>
2023-04-06 19:08:34 -0400

    set up static assets

--- server/server.js
+++ server/server.js
@@ -14,5 +14,21 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 
   
+  startApolloServer(typeDefs, resolvers);
