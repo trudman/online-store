6d31fec25bfcb48cda5e8128f27a7a452787e3d6
Iris <iridian95@live.com>
2023-04-06 20:02:10 -0400

    adding html for signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
@@ -15,7 +14,7 @@ function Signup() {
         const token = mutationResponse.data.addUser.token;
         Auth.login(token);
     };
-    
+
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormState({
@@ -23,4 +22,58 @@ function Signup() {
           [name]: value,
         });
       };
- };
+      
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First Name.."
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="Last Name..."
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="Email..."
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
+
