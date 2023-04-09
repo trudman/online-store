8a947b4bcd076af335703c732b5a7193c1a29976
Iris <iridian95@live.com>
2023-04-08 21:26:52 -0400

    delete btn

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+//Home page importing menu and all items
 const Home = () => {
   return (
     <div className="container">
